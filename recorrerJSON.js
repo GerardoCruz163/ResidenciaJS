f7f74bb87f8702b793e881eb62aa6a1834b0e92c
Gerardo Cruz <gerarojo243@gmail.com>
2024-08-16 15:08:16 -0600

    cmabios

---
+++ recorrerJSON.js
@@ -0,0 +1,11 @@
+import pdf from "./pdf.json"
+let jsonArray= JSON.parse(data);
+
+jsonArray.forEach(function(data) {
+    
+    console.log("Nombre:", data.JSON);
+    
+    
+    console.log("-----------");
+  });
+  
