328f0dd042613bf3357a7ea3cc2072c322239f4c
GerardoCruz163 <gerarojo243@gmail.com>
2024-08-23 13:31:08 -0600

    cambios de diseno

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,9 @@
 //const fetch = require('node-fetch');
 import express from 'express';
 import fetch from 'node-fetch';
+import multer from 'multer';
+import fs from 'fs';
+import path from 'path';
 
 const dotenv = await import('dotenv');
 dotenv.config();
@@ -10,29 +13,35 @@ const app = express();
 const PORT = process.env.PORT || 3021;
 
 app.use(express.json());
+const upload = multer({ dest: 'uploads/' });
 
-app.post('/extract-pdf', async(req,res)=>{
-    const{pdfUrl} = req.body;
+app.post('/extract-pdf', upload.single('pdfFile'), async(req,res)=>{
+    const pdfFilePath = req.file.path;
 
     try{
+        const pdfFileBuffer = fs.readFileSync(pdfFilePath);
         const response = await fetch('https://pdf-services-ue1.adobe.io/operation/extractpdf',{
             method: 'POST',
             headers:{
                 'Authorization': `Bearer ${process.env.ACCESS_TOKEN}`,
-                'Content-Type': 'application/json',
+                'X-API-KEY': process.env.API_KEY,
+                'Content-Type': 'application/pdf',
             },
-            body:JSON.stringify({
-                pdfUrl: pdfUrl,
-                option:{
-                    elementsToExtract: ['text', 'tables']
-                }
-            })
+            body: pdfFileBuffer,
         });
+
+        if(!response.ok){
+            const errorBody = await response.text();
+            throw new Error(`ERROR HTTP, ESTADO: ${response.status}, RESPUESTA: ${errorBody}`);
+        }
+
         const data = await response.json();
         res.status(200).json(data);
     }catch(error){
         console.error('Error:', error);
-        res.status(500).json({ error: 'Error xd' });
+        res.status(500).json({ error: error.message });
+    }finally{
+        fs.unlinkSync(pdfFilePath);
     }
 });
 
